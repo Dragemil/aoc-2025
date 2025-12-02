0c53d101610a5b626cd2e627fd4c50c8e7204032
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2025-12-02 22:06:01 +0100

    Use proper project for better IDE support

---
+++ Day00/Solution.cs
@@ -0,0 +1,12 @@
+namespace Day00;
+
+public static class Solution
+{
+    public static void Run()
+    {
+        var input = File.ReadAllLines("Day00/input.txt");
+
+        Console.WriteLine($"Solution for Day 00: :D");
+    }
+}
+
--- day00/solution.cs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/dotnet run
-
-var input = File.ReadAllLines("input.txt");
-
-Console.WriteLine($"Solution for Day 00: :D");
