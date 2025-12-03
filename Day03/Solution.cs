e3b2d11133d3bcf47ab78675ae9d21507e9c3424
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2025-12-03 21:56:40 +0100

    Day 03 part 2

--- Day03/Solution.cs
+++ Day03/Solution.cs
@@ -1,7 +1,11 @@
+using System.Text;
+
 namespace Day03;
 
 public static class Solution
 {
+    private const int BatteriesCount = 12;
+    
     public static void Run()
     {
         var input = File.ReadAllLines("Day03/input.txt");
@@ -11,15 +15,24 @@ public static class Solution
         Console.WriteLine($"Joltage sum: {joltageSum}");
     }
 
-    private static int GetMaxJoltage(string bank)
+    private static long GetMaxJoltage(string bank)
     {
-        var maxFirst = bank[..^1].Index().MaxBy(b => b.Item);
-        var maxSecond = bank[(maxFirst.Index+1) ..].Max();
+        var jb = new StringBuilder(BatteriesCount);
+        var lastBatteryIdx = -1;
+        for (var i = 0; i < BatteriesCount; i++)
+        {
+            var from = lastBatteryIdx + 1;
+            var to = bank.Length - BatteriesCount + i + 1;
+            (var battery, lastBatteryIdx) = bank[from..to]
+                .Zip(Enumerable.Range(from, to - from))
+                .MaxBy(b => b.First);
+            
+            jb.Append(battery);
+        }
 
-        var joltage = int.Parse(new([maxFirst.Item, maxSecond]));
+        var joltage = long.Parse(jb.ToString());
         
         Console.WriteLine($"Joltage: {joltage}");
-
         return joltage;
     }
 }
