756690b6fcffd8dc144b0a4eeb6f762270c5dbaa
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2025-12-04 22:05:36 +0100

    Day 04 part 2

--- Day04/Solution.cs
+++ Day04/Solution.cs
@@ -7,27 +7,39 @@ public static class Solution
     public static void Run()
     {
         var input = File.ReadAllLines("Day04/input.txt");
-        var n = input.Length;
-        var m = input[0].Length;
+        var grid = Array.ConvertAll(input, s => s.ToCharArray());
+        
+        var n = grid.Length;
+        var m = grid[0].Length;
 
         var accessibleSum = 0;
+        var prevSum = -1;
+        var it = 0;
 
-        for (var i = 0; i < n; i++)
+        while (accessibleSum != prevSum)
         {
-            for (var j = 0; j < m; j++)
+            Console.WriteLine($"Iteration {it}, sum: {accessibleSum}");
+            it++;
+            prevSum = accessibleSum;
+            
+            for (var i = 0; i < n; i++)
             {
-                if (!IsRoll(input, i, j))
+                for (var j = 0; j < m; j++)
                 {
-                    continue;
-                }
+                    if (!IsRoll(grid, i, j))
+                    {
+                        continue;
+                    }
 
-                var ii = new[] { i - 1, i, i + 1 };
-                var jj = new[] { j - 1, j, j + 1 };
-                var nghbrsCount = ii.SelectMany(_ => jj, (i, j) => (i, j)).Count(t => IsRoll(input, t.i, t.j));
+                    var ii = new[] { i - 1, i, i + 1 };
+                    var jj = new[] { j - 1, j, j + 1 };
+                    var nghbrsCount = ii.SelectMany(_ => jj, (i, j) => (i, j)).Count(t => IsRoll(grid, t.i, t.j));
 
-                if (nghbrsCount <= MaxNghbrs + 1)
-                {
-                    accessibleSum++;
+                    if (nghbrsCount <= MaxNghbrs + 1)
+                    {
+                        accessibleSum++;
+                        grid[i][j] = '.';
+                    }
                 }
             }
         }
@@ -35,7 +47,7 @@ public static class Solution
         Console.WriteLine($"Accessible sum: {accessibleSum}");
     }
 
-    private static bool IsRoll(string[] grid, int i, int j)
+    private static bool IsRoll(char[][] grid, int i, int j)
     {
         return grid.ElementAtOrDefault(i)?.ElementAtOrDefault(j) == '@';
     }
