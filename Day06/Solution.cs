5759e97d4496ec4f7c6c980e04a0a7ffb538d27f
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2025-12-06 12:32:59 +0100

    Day 06 part 2

--- Day06/Solution.cs
+++ Day06/Solution.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Day06;
 
 public static class Solution
@@ -5,27 +7,49 @@ public static class Solution
     public static void Run()
     {
         var input = File.ReadAllLines("Day06/input.txt");
-        var problems = input[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            .Select(n => new Problem{ Numbers = [long.Parse(n)]}).ToList();
-        foreach (var (number, problem) in input[1..^1]
-            .SelectMany(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Zip(problems)))
-        {
-            problem.Numbers.Add(long.Parse(number));
-        }
-        
-        foreach (var (op, problem) in input[^1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Zip(problems))
-        {
-            if (op is not [var opc])
-            {
-                throw new InvalidOperationException("Too long sign");
-            }
+        var transposed = Transpose(input);
 
-            problem.Operator = opc;
+        var problems = new List<Problem>();
+        var createNew = true;
+        foreach (var s in transposed)
+        {
+            if (createNew)
+            {
+                createNew = false;
+                problems.Add(new()
+                {
+                    Numbers = [int.Parse(s[..^1])],
+                    Operator = s[^1],
+                });
+            }
+            else if (!string.IsNullOrWhiteSpace(s))
+            {
+                problems[^1].Numbers.Add(int.Parse(s));
+            }
+            else
+            {
+                createNew = true;
+            }
         }
 
         var problemSum = problems.Sum(p => p.Compute());
 
         Console.WriteLine($"Sum of problems: {problemSum}");
+    }
+
+    private static string[] Transpose(string[] input)
+    {
+        var transposed = Enumerable.Range(0, input[0].Length).Select(_ => new StringBuilder()).ToArray();
+
+        foreach (var t in input)
+        {
+            for (int j = 0; j < input[0].Length; j++)
+            {
+                transposed[j].Append(t[j]);
+            }
+        }
+
+        return transposed.Select(sb => sb.ToString()).ToArray();
     }
 
     private class Problem
