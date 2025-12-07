1e883184676d3b0bea48ef138da96898295ba9a7
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2025-12-07 13:52:28 +0100

    Day 07 part 2

--- Day07/Solution.cs
+++ Day07/Solution.cs
@@ -6,23 +6,35 @@ public static class Solution
     {
         var input = File.ReadAllLines("Day07/input.txt");
         var beamStartIdx = input[0].IndexOf('S');
-        var beams = new HashSet<int> { beamStartIdx };
-        var splitCount = 0;
+        var beamTimelines = new Dictionary<int, long> { [beamStartIdx] = 1 };
+        long timelinesCount = 1;
         
         foreach (var layer in input[1..])
         {
-            var toSplit = beams.Where(b => layer[b] == '^').ToArray();
-            splitCount += toSplit.Length;
+            var toSplit = beamTimelines.Where(b => layer[b.Key] == '^').ToArray();
+            timelinesCount += toSplit.Sum(b => b.Value);
             
-            foreach (var b in toSplit)
+            foreach (var (beam, timelines) in toSplit)
             {
-                beams.Remove(b);
-                beams.Add(b - 1);
-                beams.Add(b + 1);
+                beamTimelines.Remove(beam);
+                beamTimelines.AddOrUpdate(beam - 1, timelines, t => t + timelines);
+                beamTimelines.AddOrUpdate(beam + 1, timelines, t => t + timelines);
             }
         }
 
-        Console.WriteLine($"Tachyon beam splits count: {splitCount}");
+        Console.WriteLine($"Tachyon beam timelines count: {timelinesCount}");
+    }
+
+    private static void AddOrUpdate<K, V>(this IDictionary<K, V> dict, K key, V toAdd, Func<V, V> toUpdate)
+    {
+        if (dict.TryGetValue(key, out var value))
+        {
+            dict[key] = toUpdate(value);
+        }
+        else
+        {
+            dict[key] = toAdd;
+        }
     }
 }
 
